4559632ba5714f8c34d053b404c415c8cbcf492d
your name your last name <soarnsoar@gmail.com>
2022-03-16 12:39:06 +0900

    a

--- example_dnn/hadd_mass.sh
+++ example_dnn/hadd_mass.sh
@@ -1,3 +1,6 @@
-M=$1
-hadd ggf${M}.root nanoLatino_GluGluHToWWToLNuQQ_M${M}_*/*.root
-hadd vbf${M}.root nanoLatino_VBFHToWWToLNuQQ_M${M}_*/*.root
+ARR_M=(115 120 124 125 126 130 135 140 145 150 155 160 165 170 175 180 190 200 210 230 250 270 300 350 400 450 500 550 600 650 700 750 800 900 1000 1500 2000 2500 3000 4000 5000)
+
+for M in ${ARR_M[@]};do
+    hadd ggf${M}.root nanoLatino_GluGluHToWWToLNuQQ_M${M}_*/*.root
+    hadd vbf${M}.root nanoLatino_VBFHToWWToLNuQQ_M${M}_*/*.root
+done
