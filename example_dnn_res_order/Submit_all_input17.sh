98a7e7190369da0fbc8ba2f164afb70f7fe1423e
your name your last name <soarnsoar@gmail.com>
2022-03-19 07:29:16 +0900

    a

---
+++ example_dnn_res_order/Submit_all_input17.sh
@@ -0,0 +1,54 @@
+export _JHCHOI_TMVATEST="${PWD}"
+
+address='root://cms-xrdr.private.lo:2094'
+#INPUTDIR=/cms_scratch/jhchoi/keras_my/input_tree
+INPUTDIR="${address}"'//xrd/store/user/jhchoi/Latino/HWWNano/Fall2017_102X_nAODv7_Full2017v7_HWW/MCl1loose2017v7__MCCorr2017v7__HMSemilepSKIMv7_1__HMFull_V13_jhchoi_nom/'
+
+
+RUNPY=${PWD}/test_input16.py
+XML=${PWD}/../xml_h5_dnn/TMVAClassification_DNN.weights.xml
+
+ARR_F=(
+nanoLatino_GluGluHToWWToLNuQQ_M4000__part0.root
+nanoLatino_GluGluHToWWToLNuQQ_M4000__part1.root
+nanoLatino_GluGluHToWWToLNuQQ_M4000__part2.root
+nanoLatino_GluGluHToWWToLNuQQ_M4000__part3.root
+nanoLatino_GluGluHToWWToLNuQQ_M4000__part4.root
+nanoLatino_GluGluHToWWToLNuQQ_M4000__part5.root
+nanoLatino_GluGluHToWWToLNuQQ_M4000__part6.root
+nanoLatino_GluGluHToWWToLNuQQ_M4000__part7.root
+nanoLatino_GluGluHToWWToLNuQQ_M4000__part8.root
+nanoLatino_GluGluHToWWToLNuQQ_M4000__part9.root
+nanoLatino_GluGluHToWWToLNuQQ_M4000__part10.root
+
+nanoLatino_VBFHToWWToLNuQQ_M4000__part0.root
+nanoLatino_VBFHToWWToLNuQQ_M4000__part1.root
+nanoLatino_VBFHToWWToLNuQQ_M4000__part2.root
+nanoLatino_VBFHToWWToLNuQQ_M4000__part3.root
+nanoLatino_VBFHToWWToLNuQQ_M4000__part4.root
+nanoLatino_VBFHToWWToLNuQQ_M4000__part5.root
+nanoLatino_VBFHToWWToLNuQQ_M4000__part6.root
+nanoLatino_VBFHToWWToLNuQQ_M4000__part7.root
+nanoLatino_VBFHToWWToLNuQQ_M4000__part8.root
+nanoLatino_VBFHToWWToLNuQQ_M4000__part9.root
+nanoLatino_VBFHToWWToLNuQQ_M4000__part10.root
+
+)
+ARR_F=()
+cd /xrootd//store/user/jhchoi/Latino/HWWNano/Fall2017_102X_nAODv7_Full2017v7_HWW/MCl1loose2017v7__MCCorr2017v7__HMSemilepSKIMv7_1__HMFull_V13_jhchoi_nom/
+ARR_F+=( $(ls nanoLatino_GluGluHToWWToLNuQQ_M*__part*.root) )
+ARR_F+=( $(ls nanoLatino_VBFHToWWToLNuQQ_M*__part*.root) )
+cd -
+
+#   parser.add_option("-c","--command",   dest="command", help="command to run")
+#   parser.add_option("-d","--workdir",   dest="workdir", help="workarea")
+#   parser.add_option("-n","--jobname",   dest="jobname", help="jobname")
+#   parser.add_option("-m","--ncpu",   dest="ncpu", help="number of multicores",default=1)
+#   parser.add_option("-s","--submit",   dest="submit",action="store_true", help="submit",default=False)
+#   parser.add_option("-r","--memory",   dest="memory", help="memory")
+
+for F in ${ARR_F[@]};do
+    #echo $F
+    python python_tool/ExportShellCondorSetup.py -c "python ${RUNPY} ${INPUTDIR}/${F} ${XML}" -d "WORKDIR/${F}" -n "testinput" -m 1 -s 
+
+done
---
+++ example_dnn_res_order/test_input16.py
@@ -0,0 +1,110 @@
+import glob
+import ROOT
+from array import array
+import sys
+import os
+
+#_JHCHOI_TMVATEST=os.environ["_JHCHOI_TMVATEST"]
+#print _JHCHOI_TMVATEST
+inputname=sys.argv[1]
+xmlfile=sys.argv[2]
+WP='DeepAK8WP0p5'
+ALGO="dMchi2Resolution"
+_ALGO_="_"+ALGO+"_"
+
+#xmlfile=_JHCHOI_TMVATEST+'../xml_h5_dnn/TMVAClassification_DNN.weights.xml'
+
+
+##---Variables---##
+AddJet_pt1=array('f',[0])
+AddJet_eta1=array('f',[0])
+AddJet_pt2=array('f',[0])
+AddJet_eta2=array('f',[0])
+max_mjj=array('f',[0])
+mjj_of_max_dEta=array('f',[0])
+max_dEta=array('f',[0])
+dEta_of_max_mjj=array('f',[0])
+lnjj_pt=array('f',[0])
+lnjj_mass=array('f',[0])
+
+##---Set TMVA--##
+
+myreader=ROOT.TMVA.Reader("V")
+myreader.AddVariable('Alt$(AddJetResol_'+ALGO+'_nom_pt[0], 3.)',AddJet_pt1)
+myreader.AddVariable('Alt$(AddJetResol_'+ALGO+'_nom_eta[0] , 3.)',AddJet_pt2)
+myreader.AddVariable('Alt$(AddJetResol_'+ALGO+'_nom_pt[1], 3.)',AddJet_eta2)
+myreader.AddVariable('Alt$(AddJetResol_'+ALGO+'_nom_eta[1] , 3.)',AddJet_eta1)
+myreader.AddVariable('max_mjj_Resol_'+ALGO+'_nom',max_mjj)
+myreader.AddVariable('mjj_of_max_dEta_Resol_'+ALGO+'_nom',mjj_of_max_dEta)
+myreader.AddVariable('max_dEta_Resol_'+ALGO+'_nom',max_dEta)
+myreader.AddVariable('dEta_of_max_mjj_Resol_'+ALGO+'_nom',dEta_of_max_mjj)
+myreader.AddVariable('lnjj_'+ALGO+'_nom_pt',lnjj_pt)
+myreader.AddVariable('lnjj_'+ALGO+'_nom_mass',lnjj_mass)
+
+from os import environ
+environ['KERAS_BACKEND'] = 'tensorflow'
+ROOT.TMVA.PyMethodBase.PyInitialize()
+
+
+myreader.BookMVA("PyKeras::DNN",xmlfile)
+
+
+
+
+
+chain_sig=ROOT.TChain('Events')
+chain_sig.Add(inputname)
+#for fsig in siginput_list:
+#    chain_sig.Add(fsig)
+
+
+h_s=ROOT.TH1D("h",'h',100,0.,1.)
+
+
+print 'run sig'
+N=chain_sig.GetEntries()
+print N
+i=0
+#  'sig' : 'Lepton_pt[0] > 40 && isBoost_'+WP+'_nom && (lnJ_'+WP+'_nom_widx >=0)&&(fabs(Lepton_eta[0]) < 2.5  ) && (  Alt$(Lepton_isLoose[1]*Lepton_pt[1],-1) < 10 ) && PuppiMET_nom_pt > 40 ',
+
+
+for event in chain_sig:
+    i+=1
+    if i%100==0 : print i,'/',N
+    lepton_pt=event.Lepton_pt[0]
+    isBoost=event.isBoost_DeepAK8WP0p5_nom
+    lnJ_nom_widx=event.lnJ_DeepAK8WP0p5_nom_widx
+    PuppiMET=event.PuppiMET_nom_pt
+
+    if lepton_pt<40 : continue
+    if not isBoost:continue
+    if lnJ_nom_widx<0:continue
+    if PuppiMET<40:continue
+    
+    AddJet_pt1[0]= event.AddJetResol_dMchi2Resolution_nom_pt[0] if len(event.AddJetBoost_DeepAK8WP0p5_nom_pt)>1 else 3.
+    AddJet_eta1[0]= event.AddJetResol_dMchi2Resolution_nom_eta[0] if len(event.AddJetBoost_DeepAK8WP0p5_nom_eta)>1 else 3.    
+
+
+    AddJet_pt2[0]=event.AddJetResol_dMchi2Resolution_nom_pt[1] if len(event.AddJetBoost_DeepAK8WP0p5_nom_pt)>2 else 3.
+    AddJet_eta2[0]=event.AddJetResol_dMchi2Resolution_nom_eta[1] if len(event.AddJetBoost_DeepAK8WP0p5_nom_eta)>2 else 3.
+    
+    max_mjj[0]=event.max_mjj_Resol_dMchi2Resolution_nom
+    max_dEta[0]=event.max_dEta_Resol_dMchi2Resolution_nom
+    
+    dEta_of_max_mjj[0]=event.dEta_of_max_mjj_Resol_dMchi2Resolution_nom
+    mjj_of_max_dEta[0]=event.mjj_of_max_dEta_Resol_dMchi2Resolution_nom
+
+    lnJ_pt[0]=event.lnjj_dMchi2Resolution_nom_pt
+    lnJ_mass[0]=event.lnjj_dMchi2Resolution_nom_mass
+
+
+    ret=myreader.EvaluateMVA("PyKeras::DNN")
+    h_s.Fill(ret)
+    #print ret
+
+mytfile=ROOT.TFile("histos.root",'RECREATE')
+mytfile.cd()
+h_s.Write()
+
+mytfile.Write()
+mytfile.Close()
